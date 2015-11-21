074139a89781f0eb1453170d7b64b69a3576d020
Pedro Algarvio <pedro@algarvio.me>
2015-11-21 18:55:41 +0000

    Add the extra log levels we use in salt

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Import python libs
+import logging
+
+if not hasattr(logging, 'TRACE'):
+    logging.TRACE = 5
+    logging.addLevelName(logging.TRACE, 'TRACE')
+if not hasattr(logging, 'GARBAGE'):
+    logging.GARBAGE = 1
+    logging.addLevelName(logging.GARBAGE, 'GARBAGE')
+
+
 pytest_plugins = 'pytester'
