194932fe0c0cce55711a86428d5aa62c5c73f590
Pedro Algarvio <pedro@algarvio.me>
2018-08-13 18:24:13 +0100

    Bump version for release

--- pytestsalt/utils.py
+++ pytestsalt/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
     :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
-    :copyright: Copyright 2015 by the SaltStack Team, see AUTHORS for more details.
+    :copyright: Copyright 2015-2018 by the SaltStack Team, see AUTHORS for more details.
     :license: Apache 2.0, see LICENSE for more details.
 
 
