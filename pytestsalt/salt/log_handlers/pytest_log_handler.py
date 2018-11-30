a131c045170eda88df78e56de4603738b1d4a67f
Pedro Algarvio <pedro@algarvio.me>
2018-11-30 19:22:36 +0000

    Update salt external log handler to latest functionality
    * If log host is not passed, try and find out based on the default
    gateway. This has only been tried so far when salt is running in a
    vagrant guest.
    * Try to connect before even trying to process log messages.
    * Make sure log messages are unicode

--- pytestsalt/salt/log_handlers/pytest_log_handler.py
+++ pytestsalt/salt/log_handlers/pytest_log_handler.py
@@ -7,7 +7,7 @@ Salt External Logging Handler
 '''
 
 # Import python libs
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
 import os
 import socket
 import threading
@@ -19,6 +19,12 @@ import msgpack
 
 # Import Salt libs
 import salt.log.setup
+try:
+    import salt.utils.stringutils
+    to_unicode = salt.utils.stringutils.to_unicode
+except ImportError:
+    import salt.utils
+    to_unicode = salt.utils.to_unicode
 
 __virtualname__ = 'pytest_log_handler'
 
@@ -32,6 +38,32 @@ def __virtual__():
 
 
 def setup_handlers():
+    host_addr = __opts__.get('pytest_log_host')
+    if not host_addr:
+        import subprocess
+        if __opts__['pytest_windows_guest'] is True:
+            proc = subprocess.Popen('ipconfig', stdout=subprocess.PIPE)
+            for line in proc.stdout.read().strip().encode(__salt_system_encoding__).splitlines():
+                if 'Default Gateway' in line:
+                    parts = line.split()
+                    host_addr = parts[-1]
+                    break
+        else:
+            proc = subprocess.Popen(
+                "netstat -rn | grep -E '^0.0.0.0|default' | awk '{ print $2 }'",
+                shell=True, stdout=subprocess.PIPE
+            )
+            host_addr = proc.stdout.read().strip().encode(__salt_system_encoding__)
+    host_port = __opts__['pytest_log_port']
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((host_addr, host_port))
+    except socket.error:
+        # Don't even bother if we can't connect
+        return
+    finally:
+        sock.close()
+
     # One million log messages is more than enough to queue.
     # Above that value, if `process_queue` can't process fast enough,
     # start dropping. This will contain a memory leak in case `process_queue`
@@ -39,10 +71,12 @@ def setup_handlers():
     queue_size = 10000000
     queue = Queue(queue_size)
     handler = salt.log.setup.QueueHandler(queue)
-    handler.setLevel(1)
+    level = salt.log.setup.LOG_LEVELS[(__opts__.get('pytest_log_level') or 'error').lower()]
+    handler.setLevel(level)
     pytest_log_prefix = os.environ.get('PYTEST_LOG_PREFIX') or __opts__['pytest_log_prefix']
     process_queue_thread = threading.Thread(target=process_queue,
-                                            args=(__opts__['pytest_log_port'],
+                                            args=(host_addr,
+                                                  host_port,
                                                   pytest_log_prefix,
                                                   queue))
     process_queue_thread.daemon = True
@@ -50,9 +84,14 @@ def setup_handlers():
     return handler
 
 
-def process_queue(port, prefix, queue):
+def process_queue(host, port, prefix, queue):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', port))
+    try:
+        sock.connect((host, port))
+    except socket.error:
+        sock.close()
+        return
+
     while True:
         try:
             record = queue.get()
@@ -62,7 +101,8 @@ def process_queue(port, prefix, queue):
             # Just send every log. Filtering will happen on the main process
             # logging handlers
             record_dict = record.__dict__
-            record_dict['msg'] = prefix + record_dict['msg']
+            record_dict['msg'] = '[{}] {}'.format(to_unicode(prefix),
+                                                  to_unicode(record_dict['msg']))
             sock.sendall(msgpack.dumps(record_dict, encoding='utf-8'))
         except (IOError, EOFError, KeyboardInterrupt, SystemExit):
             break
