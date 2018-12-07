bb2976e2fe977f1d6a3e97a0bed8b1f5376d234e
Pedro Algarvio <pedro@algarvio.me>
2018-12-07 11:13:03 +0000

    Still provide a __version_info__ attribute

--- pytestsalt/__init__.py
+++ pytestsalt/__init__.py
@@ -3,6 +3,16 @@
 PyTest Salt Plugin
 '''
 
+# Import Python libs
+import re
+# Import pytest salt libs
 from pytestsalt._version import get_versions
+
+# Store the version attribute
 __version__ = get_versions()['version']
 del get_versions
+
+# Define __version_info__ attribute
+regex = re.compile(r'(?P<year>[\d]{4})\.(?P<month>[\d]{1,2})\.(?P<day>[\d]{1,2})(?:\.dev0\+(?P<commits>[\d]+)\.(?:.*))?')
+__version_info__ = tuple([int(p) for p in regex.match(__version__).groups()])
+del regex
