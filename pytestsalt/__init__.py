26b0583bb08c9207fbcc2589aba46a8aafbab1be
Pedro Algarvio <pedro@algarvio.me>
2018-12-07 19:47:37 +0000

    Fix version info generation

--- pytestsalt/__init__.py
+++ pytestsalt/__init__.py
@@ -14,5 +14,5 @@ del get_versions
 
 # Define __version_info__ attribute
 regex = re.compile(r'(?P<year>[\d]{4})\.(?P<month>[\d]{1,2})\.(?P<day>[\d]{1,2})(?:\.dev0\+(?P<commits>[\d]+)\.(?:.*))?')
-__version_info__ = tuple([int(p) for p in regex.match(__version__).groups()])
+__version_info__ = tuple([int(p) for p in regex.match(__version__).groups() if p])
 del regex
