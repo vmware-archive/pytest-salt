57e725483eb82a7c7e25dadf2a4b54a91d3b773b
Pedro Algarvio <pedro@algarvio.me>
2019-01-02 13:51:27 +0000

    On some weird occasions we might not have a regex match.

--- pytestsalt/__init__.py
+++ pytestsalt/__init__.py
@@ -5,6 +5,7 @@ PyTest Salt Plugin
 
 # Import Python libs
 import re
+
 # Import pytest salt libs
 from pytestsalt._version import get_versions
 
@@ -13,6 +14,13 @@ __version__ = get_versions()['version']
 del get_versions
 
 # Define __version_info__ attribute
-regex = re.compile(r'(?P<year>[\d]{4})\.(?P<month>[\d]{1,2})\.(?P<day>[\d]{1,2})(?:\.dev0\+(?P<commits>[\d]+)\.(?:.*))?')
-__version_info__ = tuple([int(p) for p in regex.match(__version__).groups() if p])
-del regex
+VERSION_INFO_REGEX = re.compile(
+    r'(?P<year>[\d]{4})\.(?P<month>[\d]{1,2})\.(?P<day>[\d]{1,2})'
+    r'(?:\.dev0\+(?P<commits>[\d]+)\.(?:.*))?'
+)
+try:
+    __version_info__ = tuple([int(p) for p in VERSION_INFO_REGEX.match(__version__).groups() if p])
+except AttributeError:
+    __version_info__ = (-1, -1, -1)
+finally:
+    del VERSION_INFO_REGEX
